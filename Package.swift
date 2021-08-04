8928d295d8b02a44832c457d0db1f4c215302a21
Ashraf Ali <ashraf.ali@sky.uk>
2021-08-04 08:42:58 +0100

    update swift tools

--- Package.swift
+++ Package.swift
@@ -1,13 +1,18 @@
-//
+// swift-tools-version:5.4
 //  Package.swift
 //  KeychainAccess
 //
 //  Created by kishikawa katsumi on 2015/12/4.
 //  Copyright (c) 2015 kishikawa katsumi. All rights reserved.
 //
-
 import PackageDescription
 
 let package = Package(
-    name: "KeychainAccess"
+    name: "KeychainAccess",
+    products: [
+        .library(name: "KeychainAccess", targets: ["KeychainAccess"])
+    ],
+    targets: [
+        .target(name: "KeychainAccess", path: "Lib/KeychainAccess")
+    ]
 )
